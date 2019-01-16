a852cd7a27a05e2e77d1393224e760091a6dc444
unknown <h2hallowdy@gmail.com>
2019-01-17 00:00:30 +0700

    change to MVC model

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ var bodyParser = require('body-parser');
 
 const categoryRoute = require('./routes/category.route');
 
-
-const app = express();
 const port = 3000;
+const app = express();
+
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.render('index');
